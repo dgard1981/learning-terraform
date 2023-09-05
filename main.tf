451cc129cf15d5be790c5f4679ba2144bd2e4ae3
David Gard <43265354+dgard1981@users.noreply.github.com>
2023-09-05 15:34:38 +0100

    Use module for Security Group.

--- main.tf
+++ main.tf
@@ -24,16 +24,32 @@ resource "aws_instance" "blog" {
 
   key_name = "terraform-course"
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_security_group.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
 
+module "blog_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow HTTP and HTTPS from my IP in."
+
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
